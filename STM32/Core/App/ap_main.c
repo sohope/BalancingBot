0abc6e55553542299ddee4696681c60c708364a0
sohope <msm2570@gmail.com>
2026-03-28 20:58:14 +0900

    PTY 통합 버전(BT 동작)

--- STM32/Core/App/ap_main.c
+++ STM32/Core/App/ap_main.c
@@ -6,9 +6,18 @@ extern UART_HandleTypeDef huart2;
 void ap_init()
 {
 	GyroImuSvc_Init(&hi2c1, &huart2);
+	UART_COM_Init(&huart6);
+	Robot_Init();
 }
 
 void ap_exe()
 {
-	GyroImuSvc_Execute();
+	GyroImuSvc_Execute(); 
+	Robot_Execute();
+}
+
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
+	if (huart->Instance == USART6) {
+		UART_COM_RxEventHandler(Size);
+	}
 }
