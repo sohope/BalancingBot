fee92c6c9ad438f4c7c1388cf196685d74a32e58
sohope <msm2570@gmail.com>
2026-03-28 15:20:52 +0900

    ap_main 기본 코드 추가

---
+++ Core/App/ap_main.c
@@ -0,0 +1,17 @@
+/*
+ * ap_main.c
+ *
+ *  Created on: Mar 28, 2026
+ *      Author: sohope
+ */
+
+
+void ap_init()
+{
+
+}
+
+void ap_exe()
+{
+
+}
---
+++ Core/App/ap_main.h
@@ -0,0 +1,17 @@
+/*
+ * ap_main.h
+ *
+ *  Created on: Mar 28, 2026
+ *      Author: sohope
+ */
+
+#ifndef APP_AP_MAIN_H_
+#define APP_AP_MAIN_H_
+
+#include <stdio.h>
+#include "stm32f4xx_hal.h"
+
+void ap_init();
+void ap_exe();
+
+#endif /* APP_AP_MAIN_H_ */
